1192a587d38afe78a463e7aef235844663cbf15a
Evans Ochieng <evansochieng121@gmail.com>
2022-08-04 17:30:30 +0300

    Incomplete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// Remove  DOM 'main' node 
+const main = document.getElementById("main");
+main.remove();
+
+// Create newHeader variable pointing to 'h1'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Add id to the h1 node
+//const newHeader = document.getElementById('victory');
+document.body.append(newHeader);
+
+// Add content to the node
+newHeader.textContent = "YOUR-NAME is the champion";
